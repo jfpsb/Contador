9940f7dce305001c4739c589c8f135bbdbde322d
Felipe Borges <jfpsb_felipe2@hotmail.com>
2018-02-04 00:55:12 -0300

    Fragment TelaProduto

--- app/src/main/java/com/vandamodaintima/jfpsb/contador/MainActivity.java
+++ app/src/main/java/com/vandamodaintima/jfpsb/contador/MainActivity.java
@@ -1,25 +1,58 @@
 package com.vandamodaintima.jfpsb.contador;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private ListView mDrawerList;
     private ArrayAdapter<String> mAdapter;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerList = (ListView)findViewById(R.id.left_drawer);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerList = findViewById(R.id.left_drawer);
 
         String[] osArray = { "Contador", "Produtos", "Fornecedores", "Lojas" };
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, osArray);
 
         mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Fragment fragment = null;
+                Class fragmentClass = null;
+
+                switch (i) {
+                    case 0:
+                        fragmentClass = TelaProduto.class;
+                }
+
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+                mDrawerLayout.closeDrawer(mDrawerList);
+            }
+        });
     }
 }
