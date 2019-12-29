69d0d6a987232f9dc209c35298676c314eeb8608
jfpsb <jfpsb_felipe2@hotmail.com>
2019-12-29 12:39:54 -0300

    Colocando célula com tipo dinheiro em arquivo excel de contagem de produto Adicionando célula para mostrar valor total de cada produto

--- app/src/main/java/com/vandamodaintima/jfpsb/contador/controller/arquivo/ExcelContagemProdutoStrategy.java
+++ app/src/main/java/com/vandamodaintima/jfpsb/contador/controller/arquivo/ExcelContagemProdutoStrategy.java
@@ -7,6 +7,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -15,25 +16,41 @@ import java.util.ArrayList;
 public class ExcelContagemProdutoStrategy implements IExcelStrategy<ContagemProduto> {
     @Override
     public String escreveDados(XSSFWorkbook workbook, XSSFSheet sheet, Object lista) {
-        CellStyle cellStyle = workbook.createCellStyle();
+        CellStyle estiloCelula = workbook.createCellStyle();
 
         Font fonte = workbook.createFont();
         fonte.setFontHeightInPoints((short) 12);
 
-        cellStyle.setFont(fonte);
-        cellStyle.setAlignment(CellStyle.ALIGN_CENTER);
-        cellStyle.setVerticalAlignment(CellStyle.ALIGN_CENTER);
+        estiloCelula.setFont(fonte);
+        estiloCelula.setAlignment(CellStyle.ALIGN_CENTER);
+        estiloCelula.setVerticalAlignment(CellStyle.ALIGN_CENTER);
 
         ArrayList<ContagemProduto> contagensProduto = (ArrayList<ContagemProduto>) lista;
 
         Row[] rows = new Row[contagensProduto.size()];
 
+        CellStyle estiloCelulaMonetario = workbook.createCellStyle();
+        estiloCelulaMonetario.cloneStyleFrom(estiloCelula);
+        estiloCelulaMonetario.setDataFormat((short) 8);
+
         for (int i = 1; i <= rows.length; i++) {
             rows[i - 1] = sheet.createRow(i);
             for (int j = 0; j < ContagemProduto.getHeaders().length; j++) {
                 Cell cell = rows[i - 1].createCell(j);
-                cell.setCellStyle(cellStyle);
+                if (j == 2) {
+                    cell.setCellStyle(estiloCelulaMonetario);
+                } else {
+                    cell.setCellStyle(estiloCelula);
+                }
             }
+        }
+
+        for (int i = 1; i <= rows.length; i++) {
+            Cell cell = rows[i - 1].createCell(4);
+            cell.setCellStyle(estiloCelulaMonetario);
+            cell.setCellType(XSSFCell.CELL_TYPE_FORMULA);
+            String output = String.format("%s*%s", "C" + (i + 1), "D" + (i + 1));
+            cell.setCellFormula(output);
         }
 
         for (int i = 0; i < rows.length; i++) {
@@ -49,6 +66,7 @@ public class ExcelContagemProdutoStrategy implements IExcelStrategy<ContagemProd
         sheet.setColumnWidth(1, 70 * 256);
         sheet.setColumnWidth(2, 20 * 256);
         sheet.setColumnWidth(3, 20 * 256);
+        sheet.setColumnWidth(4, 30 * 256);
 
         return "Contagem.xlsx";
     }
